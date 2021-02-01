781c33110ee08bf6e3f91f7267674992288efdbf
SpiroC <spiroc@gmail.com>
2021-02-01 22:01:22 +1100

    Mopidy and upmpdcli

---
+++ mopidyupmp/setup-files/mopidy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Copy config if it does not already exist
+if [ ! -f /mopidy/mopidy.conf ]; then
+    cp /setup-files/mopidy.conf /mopidy/mopidy.conf
+fi
+
+if [ ${APT_PACKAGES:+x} ]; then
+    echo "-- INSTALLING APT PACKAGES $APT_PACKAGES --"
+    sudo apt-get install -y $APT_PACKAGES
+fi
+if  [ ${PIP_PACKAGES:+x} ]; then
+    echo "-- INSTALLING PIP PACKAGES $PIP_PACKAGES --"
+    pip3 install $PIP_PACKAGES
+fi
+if [ ${UPDATE:+x} ]; then
+    echo "-- UPDATING ALL PACKAGES --"
+    sudo apt-get update
+    sudo apt-get upgrade -y
+    pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U # Upgrade all pip packages
+fi
+
+exec mopidy --config /mopidy/mopidy.conf "$@"
